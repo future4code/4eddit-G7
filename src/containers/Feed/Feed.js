ee3469a5cad20c789e2b1d3dc47467ba055e8393
caiobrian <caiogomeslog@gmail.com>
2019-11-12 13:59:16 -0300

    parte estática básica, feita

--- src/containers/Feed/Feed.js
+++ src/containers/Feed/Feed.js
@@ -2,33 +2,70 @@ import React from "react";
 import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import { routes } from "../Router/index";
+import {
+	PostCard, HeaderPost, FooterPost,
+	Comments, Thumbs, PostContainer,
+	NewPostContainer, TextContainer,
+	TextItem, StyledTextArea, StyledTitle,
+	ButtonPost, ThumbArrow
+} from "./style";
+import ArrowUp from './img/arrowUp.png'
+import ArrowDown from './img/arrowDown.png'
 
 class Feed extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
+	constructor(props) {
+		super(props);
+		this.state = {
+		};
+	};
 
-        };
-    };
-
-    render () {
-        return (
-            <div>
-                <button onClick={this.props.goToPost}>Post</button>
-            </div>
-        );
-    };
+	render() {
+		return (
+			<PostContainer>
+				<NewPostContainer>
+					<StyledTitle>Faça um novo Post!</StyledTitle>
+					<StyledTextArea
+						label="O que você deseja postar?"
+						multiline
+						rowsMax="5"
+						margin="normal"
+						variant="outlined"
+					/>
+					<ButtonPost>POSTAR</ButtonPost>
+				</NewPostContainer>
+				<PostCard>
+					<HeaderPost>
+						<TextItem>Nome do usuário</TextItem>
+					</HeaderPost>
+					<TextContainer>
+						<TextItem>Mussum Ipsum, cacilds vidis litro abertis. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.
+							Quem num gosta di mim que vai caçá sua turmis! </TextItem>
+					</TextContainer>
+					<FooterPost>
+						<Thumbs>
+							<ThumbArrow src={ArrowUp} />
+							0
+							<ThumbArrow src={ArrowDown} />
+						</Thumbs>
+						<Comments>
+							<TextItem> 0 comentários</TextItem>
+						</Comments>
+					</FooterPost>
+				</PostCard>
+			</PostContainer>
+		);
+	};
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        goToPost: () => {dispatch(push(routes.post))},
-    };
+	return {
+		goToPost: () => { dispatch(push(routes.post)) },
+	};
 };
 
 
 
-export default connect (
-    null, 
-    mapDispatchToProps
-    )(Feed)
+export default connect(
+	null,
+	mapDispatchToProps
+)(Feed)

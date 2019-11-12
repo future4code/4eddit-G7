3de7b00158094028ee1ab18872eafe2e46ecd7b2
Samuel <squoliver@gmail.com>
2019-11-12 15:45:46 -0300

    Layout estático para as telas de login e de cadastro - OK

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,6 +2,45 @@ import React from "react";
 import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import { routes } from "../Router/index";
+import styled from "styled-components";
+import TextField from '@material-ui/core/TextField';
+import { Button, FormControl } from '@material-ui/core';
+
+const HomePageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 90%;
+    margin: auto;
+    min-width: 900px;
+`
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 25%;
+  margin: auto;  
+  border: 1px solid black;
+  border-radius: 5px;
+`
+
+const StyledFormControl = styled(FormControl)`
+    display: flex;
+    flex-direction: column;
+    padding: 2% 5%;
+`
+
+const StyledButton = styled(Button)`
+    margin: 5%;
+`
+
+const Buttons = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    margin-bottom: 30px;
+`
 
 class HomePage extends React.Component {
     constructor (props) {
@@ -11,12 +50,25 @@ class HomePage extends React.Component {
         };
     };
 
+    teste = () =>{
+        console.log("teste");
+    }
+
     render () {
         return (
-            <div>
-                <button onClick={this.props.goToRegister}>Cadastro</button>
-                <button onClick={this.props.goToFeed}>Feed</button>
-            </div>
+            <HomePageWrapper>
+                <StyledForm>
+                    <StyledFormControl onSubmit={this.teste}>
+                        <h2>Bem vindo ao Feddit!</h2>
+                        <TextField fullWidth required name="email" type="email" required label="E-mail" />
+                        <TextField fullWidth required name="password" type="password" label="Senha" />
+                    </StyledFormControl>
+                    <Buttons>
+                        <StyledButton variant="contained" type="submit" color="primary" onClick={this.props.goToFeed}>Entrar</StyledButton>
+                        <StyledButton variant="contained" color="primary" onClick={this.props.goToRegister}>Cadastrar</StyledButton>
+                    </Buttons>
+                </StyledForm>
+            </HomePageWrapper>
         );
     };
 };

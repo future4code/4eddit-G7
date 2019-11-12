1a35d61a0424cefb9b5f04cce5f71ff31c42bc39
Pedro Severo <phsevero94@gmail.com>
2019-11-12 19:34:06 -0300

    separando o style dos componentes

---
+++ src/components/BoxFooter/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const BoxFooterWrapper = styled.div ` 
+    padding: 0 10px 0 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-style: solid none none none;
+    border-width: thin;
+    height: 5vh;
+    width: 100%;
+`
+
+export const ReactionButtons = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const Button = styled.button `
+    height: 100%;
+`

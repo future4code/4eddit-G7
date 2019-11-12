c7e264be2939f88bd6ab135914f8a040db360b02
Samuel <squoliver@gmail.com>
2019-11-12 10:50:33 -0300

    Alinhando o começo, com o arquivo .gitignore

--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
 
-const routes = {};
+const routes = {
+  
+};
 
 function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
-        <Route path={routes.root} component={() => <div />} />
+        <Route path={routes.root} component={() => <div/>} />
       </Switch>
     </ConnectedRouter>
   );
